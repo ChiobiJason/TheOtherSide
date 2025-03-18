6d28ed1db4a9c9b6995772ea49f9883b263260c2
c h e r i e <83512735+cherieeze@users.noreply.github.com>
2025-03-17 20:26:58 -0600

    order of screens

---
+++ app/src/main/java/com/example/theotherside/Screen1.java
@@ -0,0 +1,41 @@
+package com.example.theotherside;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+import android.media.MediaPlayer;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Screen1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen1);
+
+        VideoView videoView = findViewById(R.id.videoView);
+
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.screen1_video;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
+
+        videoView.start();
+
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                // video's done
+                // start the next screen (ScreenJokePunchline1)
+                Intent intent = new Intent(Screen1.this, ScreenJokePunchline1.class);
+                startActivity(intent);
+                finish(); //then close Screen1
+            }
+        });
+    }
+}
