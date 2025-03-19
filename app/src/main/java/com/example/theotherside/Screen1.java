408d804743a8c5e33b5abcf3d73d9f30dd76ebb6
c h e r i e <83512735+cherieeze@users.noreply.github.com>
2025-03-19 12:04:33 -0600

    NOT perfect; intro montage as one video

--- app/src/main/java/com/example/theotherside/Screen1.java
+++ app/src/main/java/com/example/theotherside/Screen1.java
@@ -3,6 +3,8 @@ package com.example.theotherside;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.MediaController;
 import android.widget.VideoView;
 import android.media.MediaPlayer;
@@ -10,12 +12,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Screen1 extends AppCompatActivity {
 
+    private VideoView videoView;
+    private ImageButton muteButton;
+    private Button skipIntroButton;
+    private boolean isMuted = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.screen_joke_setup);
 
         VideoView videoView = findViewById(R.id.videoView);
+        ImageButton muteButton = findViewById(R.id.muteButton);
+
 
         String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.screen1_video;
         Uri uri = Uri.parse(videoPath);
@@ -32,10 +41,25 @@ public class Screen1 extends AppCompatActivity {
             public void onCompletion(MediaPlayer mp) {
                 // video's done
                 // start the next screen (ScreenJokePunchline1)
-                Intent intent = new Intent(Screen1.this, ScreenJokePunchline1.class);
+                Intent intent = new Intent(Screen1.this, ScreenTitle.class);
                 startActivity(intent);
                 finish(); //then close Screen1
             }
         });
+
+
+//        muteButton.setOnClickListener(v -> {
+//            if (isMuted) {
+//                // unmute
+//                videoView.setVolume(1.0f, 1.0f); // Set volume to max
+//                muteButton.setImageResource(R.drawable.ic_volume_up);
+//            } else {
+//                // Mute
+//                videoView.setVolume(0.0f, 0.0f); // Set volume to 0
+//                muteButton.setImageResource(R.drawable.ic_volume_off);
+//            }
+//            isMuted = !isMuted;
+//        });
+
     }
 }
