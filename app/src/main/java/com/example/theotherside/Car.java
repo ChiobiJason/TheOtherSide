7a585e1ca50d21cbc0821d7d02de61105d53513c
ChiobiJason <chisomchiobi@gmail.com>
2025-04-02 19:03:31 -0600

    Refactored Car Class

--- app/src/main/java/com/example/theotherside/Car.java
+++ app/src/main/java/com/example/theotherside/Car.java
@@ -6,49 +6,49 @@ import android.graphics.BitmapFactory;
 import java.util.Random;
 
 public class Car extends GameObject {
-private static Random random = new Random();
-private static final float FIXED_SPEED = 8;
+    private static Random random = new Random();
+    private static final float FIXED_SPEED = 8;
 
-public Car(Context context, float screenWidth, float screenHeight, int laneCount, int carType) {
-super(0, 0, BitmapFactory.decodeResource(context.getResources(),
+    public Car(Context context, float screenWidth, float screenHeight, int laneCount, int carType) {
+        super(0, 0, BitmapFactory.decodeResource(context.getResources(),
                 getCarResourceId(carType)));
 
-float laneWidth = screenWidth / laneCount;
+        float laneWidth = screenWidth / laneCount;
 
-// Random lane
-int lane = random.nextInt(laneCount);
+        // Random lane
+        int lane = random.nextInt(laneCount);
 
-// Position horizontally in lane
-this.x = lane * laneWidth + (laneWidth - width) / 2;
+        // Position horizontally in lane
+        this.x = lane * laneWidth + (laneWidth - width) / 2;
 
-// Start above screen
-this.y = -height;
+        // Start above screen
+        this.y = -height;
 
-// Random speed between 5 and 15
-this.speed = FIXED_SPEED;
+        // Random speed between 5 and 15
+        this.speed = FIXED_SPEED;
 
-update();
-}
+        update();
+    }
 
-private static int getCarResourceId(int carType) {
-switch (carType % 8) {
-case 0: return R.drawable.car1;
-case 1: return R.drawable.car2;
-case 2: return R.drawable.car3;
-case 3: return R.drawable.car4;
-case 4: return R.drawable.car5;
-case 5: return R.drawable.car6;
-case 6: return R.drawable.car7;
-default: return R.drawable.car8;
-}
-}
+    private static int getCarResourceId(int carType) {
+        switch (carType % 8) {
+            case 0: return R.drawable.car1;
+            case 1: return R.drawable.car2;
+            case 2: return R.drawable.car3;
+            case 3: return R.drawable.car4;
+            case 4: return R.drawable.car5;
+            case 5: return R.drawable.car6;
+            case 6: return R.drawable.car7;
+            default: return R.drawable.car8;
+        }
+    }
 
-public void update() {
-y += speed;
-super.update();
-}
+    public void update() {
+        y += speed;
+        super.update();
+    }
 
-public boolean isOffScreen(float screenHeight) {
-return y > screenHeight;
-}
+    public boolean isOffScreen(float screenHeight) {
+        return y > screenHeight;
+    }
 }
